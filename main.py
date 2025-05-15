b12eca781ae8c13c22c346da63170d029f1ce4ba
lucas-fpaz <lucas.paz@poli.ufrj.br>
2025-05-14 21:12:23 -0300

    Modificando grupos e adicionando palavras-chave

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ nlp = spacy.load("pt_core_news_lg")
 
 # Frases de referência para cada INTENT
 intents_examples = {
-    "1": ["quero ver meu histórico escolar", "histórico", "cursadas",  "disciplinas que já cursei", "lista de matérias feitas"],
-    "2": ["qual o horário das aulas", "horário", "horas", "ver calendário das aulas", "quando são minhas aulas" , "quando"],
-    "3": ["ver minhas notas", "notas", "provas", "quero saber minhas provas", "quanto tirei na avaliação", "desempenho"],
-    "4": ["preciso de um comprovante de matrícula", "matrícula", "quero declaração da faculdade", "declaração de vínculo"],
-    "5": ["trancar uma matéria", "trancar", "trancamento", "informações sobre trancamento", "trancar disciplina"],
-    "6": ["datas importantes do período", "quais são os prazos", "informações do calendário acadêmico"],
-    "7": ["quero falar com a secretaria", "secretária", "preciso de ajuda com atendimento", "entrar em contato com a secretaria"],
-    "8": ["dúvidas sobre estágio", "estágio",  "como funciona o estágio obrigatório", "orientações de estágio"],
-    "9": ["como conseguir bolsa de permanência", "bolsa",  "ajuda financeira estudantil", "bolsas para continuar o curso"],
-    "10": ["quero ser monitor", "informações sobre iniciação científica", "bolsa IC ou monitoria", "monitoria", "bolsa", "iniciação científica", "ic"]
+    "1": ["quero ver meu histórico escolar", "histórico", "cursadas",  "disciplinas que já cursei", "lista de matérias feitas", "histórico escolar", "CR", "CRA", "coeficiente de rendimento", "qual o meu CR"],
+    "2": ["qual o horário das aulas", "horário", "que horas", "ver calendário das aulas", "quando são minhas aulas" , "horários das aulas", "que horas são minhas aulas", "horário das disciplinas", "horário do semestre", "horário do curso", "horário do período"],
+    "3": ["ver minhas notas", "notas", "provas"  "quero saber minhas provas", "quanto tirei na avaliação", "desempenho", "notas das disciplinas", "notas do semestre", "notas do curso", "notas do período", "notas do histórico"],
+    "4": ["preciso de um comprovante de matrícula", "matrícula", "quero declaração da faculdade", "declaração de vínculo", "comprovante de matrícula", "declaração de matrícula", "declaração", "comprovante"],
+    "5": ["trancar uma matéria", "trancar", "trancamento", "informações sobre trancamento", "trancar disciplina", "suspender matéria",  "anular matéria", "desistir de uma matéria", "desistir de disciplina", "trancar matrícula"],
+    "6": ["datas importantes do período", "calendário acadêmico", "datas das aulas",  "quais são os prazos", "informações do calendário acadêmico" , "datas", "calendário", "feriados", "dias", "quando acaba o período", "datas importantes", "datas do semestre", "datas do curso", "datas do período"],
+    "7": ["quero falar com a secretária", "secretária", "preciso de ajuda com atendimento", "entrar em contato com a secretaria", "atendimento", "acessar a secretaria", "falar com a secretaria", "secretaria", "falar com alguém da secretaria", "falar com a coordenação", "falar com o coordenador", "falar com alguém da coordenação"],
+    "8": ["dúvidas sobre estágio", "estágio",  "como funciona o estágio obrigatório", "orientações de estágio", "fazer estágio", "estágio obrigatório", "estágio não obrigatório", "estágio curricular", "estágio supervisionado", "estagio", "estagiar", "dinheiro"],
+    "9": ["como conseguir bolsa de permanência", "bolsa estudantil",  "ajuda financeira estudantil", "bolsas para continuar o curso", "ajuda financeira", "bolsa de permanência", "bolsa de estudo", "bolsa de assistência estudantil", "bolsa de permanência"],
+    "10":["quero ser monitor", "informações sobre iniciação científica", "bolsa IC ou monitoria", "monitoria", "iniciação científica","ic", "informações sobre ic", "monitoria", "bolsa de monitoria", "bolsa de iniciação científica"]
 }
 
 intents_map = {
@@ -33,8 +33,8 @@ intents_map = {
 # Tópicos gerais agrupando intents
 grupos = {
     "matérias": ["2", "3"],
-    "matricula": ["1", "4", "5"],
-    "datas": ["6"],
+    "matricula": ["1", "4"],
+    "datas": ["5", "6"],
     "bolsa": ["8", "9", "10"],
     "secretaria": ["7"]
 }
